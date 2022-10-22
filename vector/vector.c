b3e29f85ff2e80e0cf878e2bcb06bdc7ee0dd849
Vafo <v.safokhujaev@student.inha.uz>
2022-10-22 01:42:37 +0000

    Implemented initialization of vector, basic addition
    Related to #1
    Closes #2

--- vector/vector.c
+++ vector/vector.c
@@ -1,2 +1,28 @@
 #include "vector.h"
 
+
+vector_t vector_init(int els[VECTOR_SIZE])
+{
+    int i;
+    vector_t result;
+    for(i = 0; i < VECTOR_SIZE; i++)
+    {
+        result.elements[i] = els[i];
+    }
+
+    return result;
+}
+
+vector_t vector_copy(vector_t source);
+
+vector_t vector_add(vector_t a, vector_t b)
+{
+    int i;
+    vector_t result;
+    for(i = 0; i < VECTOR_SIZE; i++)
+    {
+        result.elements[i] = a.elements[i] + b.elements[i];
+    }
+
+    return result;
+}
--- vector/vector.h
+++ vector/vector.h
@@ -10,5 +10,9 @@ typedef struct vector_t{
     vector_el elements[VECTOR_SIZE];
 } vector_t;
 
+vector_t vector_init(int els[VECTOR_SIZE]);     // Initialization using existing array of size VECTOR_SIZE
+vector_t vector_copy(vector_t source);          // Initialization using existing vector_t (Might be used in future in case of pointers)
+vector_t vector_add(vector_t a, vector_t b);    // Addition over 2 vectors
+
 
 #endif
