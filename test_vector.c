4c03e45662fba6c025204ea7b19a42a45ff8db89
Vafo <v.safokhujaev@student.inha.uz>
2022-10-22 04:41:40 +0000

    Implemented cross product only for 3d vecs
    Closes #9

--- test_vector.c
+++ test_vector.c
@@ -10,6 +10,9 @@ int main(int argc, char *argv[])
     vector_t first, second, result;
     int els1[] = {1, 2, 3};
     int els2[] = {4, 5, 6};
+    int e1[] = {1, 0, 0};
+    int e2[] = {0, 1, 0};
+
     first = vector_init(els1);
     second = vector_init(els2);
     
@@ -37,6 +40,16 @@ int main(int argc, char *argv[])
     printf(".\n");
     print_vector(second);
     printf("= %d\n", result_el);
+
+    // Testing cross product
+    printf("\nTesting cross product\n");
+    first = vector_init(e1);
+    second = vector_init(e2);
+    result = vector_cross(first, second);
+    print_vector(first);
+    printf("x\n");
+    print_vector(second);
+    print_vector(result);
 }
 
 void print_vector(vector_t src)
