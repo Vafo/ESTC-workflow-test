d20fc5350cdb720156158dbb68e89b4dd71cb51b
Vafo <v.safokhujaev@student.inha.uz>
2022-10-22 01:54:41 +0000

    Implemented vector substaction
    Closes #5

--- vector/vector.c
+++ vector/vector.c
@@ -25,6 +25,7 @@ vector_t vector_copy(vector_t source)
     return result; 
 }
 
+
 vector_t vector_add(vector_t a, vector_t b)
 {
     int i;
@@ -32,6 +33,19 @@ vector_t vector_add(vector_t a, vector_t b)
     for(i = 0; i < VECTOR_SIZE; i++)
     {
         result.elements[i] = a.elements[i] + b.elements[i];
+    }
+
+    return result;
+}
+
+
+vector_t vector_sub(vector_t a, vector_t b)
+{
+    int i;
+    vector_t result;
+    for(i = 0; i < VECTOR_SIZE; i++)
+    {
+        result.elements[i] = a.elements[i] - b.elements[i];
     }
 
     return result;
